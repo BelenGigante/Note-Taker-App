7a90593c9c5ac413c76d9c136a91043e1a3cb54d
BelenGigante <vinn_a@hotmail.com>
2023-01-15 11:46:31 -0500

    typed delete code

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-const { randomBytes } = require('crypto');
 const express = require('express');
 const fs = require('fs');
-const { dirname } = require('path');
 const path = require('path');
 
 const PORT = process.env.PORT || 3001;
@@ -12,12 +10,18 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/api/notes', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,'./public/index.html'));
 });
 
 app.get('/notes', (req,res)=> {
     res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req,res) =>{
+    const data = fs.readFileSync('./db/db.json', 'utf8');
+    const remindersPar = JSON.parse(data);
+    res.json(remindersPar);
 });
 
 const uui = () => {
@@ -30,12 +34,21 @@ app.post('/api/notes', (req,res) => {
     const remindersPar = JSON.parse(data);
     const newReminder = {
         ...req.body,
-        id: uui()
+        reminderId: uui()
     };
     remindersPar.push(newReminder);
     const  remindersStr = JSON.stringify(notes,null,2);
     fs.writeFileSync('./bd/bd.json', remindersStr);
     res.json('Reminder Saved');
-})
+});
+
+app.delete('/api/notes/:reminderId',(req,res) =>{
+    const data = fs.readFileSync('./db/db.json','utf8');
+    // const remindersPar = JSON.parse(data).filter(note=> note.id === req.params.reminderId);
+    const  remindersStr = JSON.stringify(notes,null,2);
+    fs.writeFileSync('./db/db.json', remindersStr);
+    res.json('Reminder Deleted');
+});
+
 
 app.listen(PORT);
